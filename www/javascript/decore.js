a5fd8a6591ba196ecf685e33dbd1b5d4d2e58d36
walaa111 <walaagamil18@gmail.com>
2017-01-23 23:57:05 +0200

    Update decore.js

--- www/javascript/decore.js
+++ www/javascript/decore.js
@@ -61,5 +61,41 @@ $('a').magnificPopup({
 
 
 
+var admobid = {};
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-7251676025279948~2665838710',
+        interstitial: 'ca-app-pub-7251676025279948/4142571918'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-6869992474017983/4806197152',
+        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-6869992474017983/8878394753',
+        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+    };
+}
+
+function initApp() {
+    if (AdMob) {
+        AdMob.createBanner({
+            adId : admobid.banner,
+            position : AdMob.AD_POSITION.BOTTOM_CENTER,
+            autoShow : true
+        });
+    }
+}
+
+document.addEventListener('deviceready', initApp, false);
+
+    // $(".ser-link").on("click",function(e){
+    //     e.preventDefault();
+    //     var ser_id=$(this).attr("ser_id");
+    //     alert(ser_id);
+    //     window.location.href("ceaser.html");
+    // });
 
 
